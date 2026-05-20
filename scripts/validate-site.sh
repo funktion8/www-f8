8bfb44dc364c53ae4cd8ccabf070cdd28296e824
ofunk <ofunk@nvidia.com>
2026-05-20 20:53:33 +0200

    Turn Funktion8 into image homepage

--- scripts/validate-site.sh
+++ scripts/validate-site.sh
@@ -10,6 +10,7 @@ test -f datenschutz.html
 test -f en/index.html
 test -f assets/site.css
 test -f assets/site.js
+test -f assets/funktion8-field.svg
 test -f styleguide/README.md
 test -f styleguide/tokens.css
 
@@ -20,7 +21,10 @@ grep -q '<link rel="alternate" hreflang="en" href="https://funktion8.de/en/">' i
 grep -q 'Organisation as Code' index.html
 grep -q 'gemeinnütz' index.html
 grep -q 'notariat8.de' index.html
-grep -q 'Funktion8 Styleguide' index.html
+grep -q 'Eine gemeinnützige Marke' index.html
+grep -q 'CI-Quelle' index.html
+grep -q 'funktion8-field.svg' index.html
+grep -q 'funktion8-field.svg' en/index.html
 grep -q 'Organisation as Code' en/index.html
 
 grep -q -- '--f8-ink' styleguide/tokens.css
@@ -28,6 +32,7 @@ grep -q 'verbindliche CI-Quelle' styleguide/README.md
 grep -q '@import url("../styleguide/tokens.css")' assets/site.css
 test ! -e assets/nac-control-plane.png
 ! grep -q 'nac-control-plane' assets/site.css
+! grep -R 'Blog\|blog\|Dokumentation\|Doku' index.html en/index.html
 ! grep -R '\bNoC\b' index.html en/index.html README.md styleguide/README.md assets/site.css
 ! grep -R 'github.com/ofunk/NaC\|github.com/ofunk' index.html en/index.html
 ! grep -RE 'fuer|Fuer|ueber|Ueber|pruef|Pruef|Oberflaeche|Aender|aender|Ausfuehr|fuehrt|Fuehr|Arbeitsflaechen|Naechster|geprueft|Begruendung|Zurueck|ergaenz|muessen|enthaelt|vollstaendigt|Veroeffentlichung|ladungsfaehige|zusaetzlichen|veroeffentlichen|Primaer|Geschaeft|verfueg|duerf|gehoer|Flaechen|Abstaende|Spaeter|koennen|moeglich|Steuerbuero|Traeger|Gruendung|schuetz|loesung|Qualitaet' index.html impressum.html datenschutz.html 404.html README.md styleguide/README.md
