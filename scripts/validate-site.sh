b3a94edd924cf444650d04290e2ef9d0a607a4fd
Oliver Funk <ofunk@funktion8.de>
2026-05-23 07:55:51 +0100

    Use central f8 brand asset

--- scripts/validate-site.sh
+++ scripts/validate-site.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+assert_absent() {
+  local pattern="$1"
+  shift
+
+  if grep -R "$pattern" "$@"; then
+    exit 1
+  fi
+}
+
+assert_absent_extended() {
+  local pattern="$1"
+  shift
+
+  if grep -RE "$pattern" "$@"; then
+    exit 1
+  fi
+}
+
 test -f index.html
 test -f CNAME
 test -f robots.txt
@@ -19,6 +37,28 @@ test -f assets/site.js
 test -f assets/funktion8-field.svg
 test -f styleguide/README.md
 test -f styleguide/tokens.css
+test ! -e assets/favicon.svg
+
+f8_brand_icon_url='https://bild8.de/assets/8/svg/f8.svg'
+for html_file in \
+  index.html \
+  impressum.html \
+  datenschutz.html \
+  kontakt.html \
+  transparenz.html \
+  gemeinnuetzigkeit.html \
+  foerderung.html \
+  404.html \
+  en/index.html \
+  en/imprint.html \
+  en/privacy.html
+do
+  grep -q "<link rel=\"icon\" href=\"${f8_brand_icon_url}\" type=\"image/svg+xml\">" "$html_file"
+done
+assert_absent 'assets/favicon.svg' index.html impressum.html datenschutz.html kontakt.html transparenz.html gemeinnuetzigkeit.html foerderung.html 404.html en/index.html en/imprint.html en/privacy.html
+assert_absent '<span class="brand-mark"' index.html impressum.html datenschutz.html kontakt.html transparenz.html gemeinnuetzigkeit.html foerderung.html en/index.html en/imprint.html en/privacy.html
+grep -q "<img class=\"brand-mark\" src=\"${f8_brand_icon_url}\" alt=\"\" width=\"44\" height=\"44\" aria-hidden=\"true\">" index.html
+grep -q "<img class=\"brand-mark\" src=\"${f8_brand_icon_url}\" alt=\"\" width=\"44\" height=\"44\" aria-hidden=\"true\">" en/index.html
 
 grep -qx 'funktion8.de' CNAME
 grep -q '<link rel="canonical" href="https://funktion8.de/">' index.html
@@ -31,6 +71,7 @@ grep -q 'Eine gemeinnützige Marke' index.html
 grep -q 'CI-Quelle' index.html
 grep -q 'funktion8-field.svg' index.html
 grep -q 'funktion8-field.svg' en/index.html
+assert_absent '>F8</text>' assets/funktion8-field.svg
 grep -q 'Organisation as Code' en/index.html
 grep -q 'Funktion8 gGmbH in Gründung' impressum.html
 grep -q 'Oliver Funk' impressum.html
@@ -51,8 +92,8 @@ grep -q -- '--f8-ink' styleguide/tokens.css
 grep -q 'verbindliche CI-Quelle' styleguide/README.md
 grep -q '@import url("../styleguide/tokens.css")' assets/site.css
 test ! -e assets/nac-control-plane.png
-! grep -q 'nac-control-plane' assets/site.css
-! grep -R 'Blog\|blog\|Dokumentation\|Doku' index.html en/index.html
-! grep -R '\bNoC\b' index.html en/index.html README.md styleguide/README.md assets/site.css
-! grep -R 'github.com/ofunk/NaC\|github.com/ofunk' index.html en/index.html
-! grep -RE 'fuer|Fuer|ueber|Ueber|pruef|Pruef|Oberflaeche|Aender|aender|Ausfuehr|fuehrt|Fuehr|Arbeitsflaechen|Naechster|geprueft|Begruendung|Zurueck|ergaenz|muessen|enthaelt|vollstaendigt|Veroeffentlichung|ladungsfaehige|zusaetzlichen|veroeffentlichen|Primaer|Geschaeft|verfueg|duerf|gehoer|Flaechen|Abstaende|Spaeter|koennen|moeglich|Steuerbuero|Traeger|Gruendung|schuetz|loesung|Qualitaet' index.html impressum.html datenschutz.html kontakt.html transparenz.html gemeinnuetzigkeit.html foerderung.html 404.html README.md styleguide/README.md
+assert_absent 'nac-control-plane' assets/site.css
+assert_absent 'Blog\|blog\|Dokumentation\|Doku' index.html en/index.html
+assert_absent '\bNoC\b' index.html en/index.html README.md styleguide/README.md assets/site.css
+assert_absent 'github.com/ofunk/NaC\|github.com/ofunk' index.html en/index.html
+assert_absent_extended 'fuer|Fuer|ueber|Ueber|pruef|Pruef|Oberflaeche|Aender|aender|Ausfuehr|fuehrt|Fuehr|Arbeitsflaechen|Naechster|geprueft|Begruendung|Zurueck|ergaenz|muessen|enthaelt|vollstaendigt|Veroeffentlichung|ladungsfaehige|zusaetzlichen|veroeffentlichen|Primaer|Geschaeft|verfueg|duerf|gehoer|Flaechen|Abstaende|Spaeter|koennen|moeglich|Steuerbuero|Traeger|Gruendung|schuetz|loesung|Qualitaet' index.html impressum.html datenschutz.html kontakt.html transparenz.html gemeinnuetzigkeit.html foerderung.html 404.html README.md styleguide/README.md
